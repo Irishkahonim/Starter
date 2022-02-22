8977fd20f629cff88f3026a8bc6bd860dd032077
iroda.burhon <iroda.kayumova@gmail.com>
2022-02-21 21:10:09 -0500

    TG-377_CreateInventoryRESTController

--- starter/src/main/java/com/starter/fullstack/rest/InventoryController.java
+++ starter/src/main/java/com/starter/fullstack/rest/InventoryController.java
@@ -3,10 +3,17 @@ package com.starter.fullstack.rest;
 import com.starter.fullstack.api.Inventory;
 import com.starter.fullstack.dao.InventoryDAO;
 import java.util.List;
+import javax.validation.Valid;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+/**import org.springframework.web.bind.annotation.DeleteMapping;
+ * 
+*/
 
 /**
  * Inventory Controller.
@@ -16,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class InventoryController {
   private final InventoryDAO inventoryDAO;
 
-  /**
+  /** 
    * Default Constructor.
    * @param inventoryDAO inventoryDAO.
    */
@@ -33,5 +40,28 @@ public class InventoryController {
   public List<Inventory> findInventories() {
     return this.inventoryDAO.findAll();
   }
+  
+  /**
+   * Everything below this line is done by IB. Need confirmation. 
+   * Save Inventory.
+   * @param inventory inventory.
+   * @return Inventory.
+   */
+  @PostMapping
+  public Inventory saveInventory(@Valid @RequestBody Inventory inventory) {
+    return this.inventoryDAO.create(inventory);
+  }
+
+  /**
+   * Delete Inventory By Id.
+   *
+   * @param ids ids.
+   */
+//  @DeleteMapping("/inventory")
+//  public void deleteInventoryById(@RequestBody List<String> ids) {
+//    Assert.notEmpty(ids, "Inventory Ids were not provided");
+//    this.inventoryDAO.deleteInventoryByIdIn(ids);
+//  } 
+  
 }
 
