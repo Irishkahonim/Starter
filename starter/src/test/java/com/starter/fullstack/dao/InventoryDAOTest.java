7f8b1963956a79ecc90fb194f1ff8e1001b85c9f
iroda.burhon <iroda.kayumova@gmail.com>
2022-03-13 12:26:57 -0400

    Commit for TG-438_DeleteDAO including Unit Test

--- starter/src/test/java/com/starter/fullstack/dao/InventoryDAOTest.java
+++ starter/src/test/java/com/starter/fullstack/dao/InventoryDAOTest.java
@@ -21,94 +21,88 @@ import org.springframework.test.context.junit4.SpringRunner;
 @DataMongoTest
 @RunWith(SpringRunner.class)
 public class InventoryDAOTest {
-	@Resource
-	private MongoTemplate mongoTemplate;
-	private InventoryDAO inventoryDAO;
-	private static final String NAME = "Amber";
-	private static final String PRODUCT_TYPE = "hops";
+  @Resource
+  private MongoTemplate mongoTemplate;
+  private InventoryDAO inventoryDAO;
+  private static final String NAME = "Amber";
+  private static final String PRODUCT_TYPE = "hops";
 
-	@Before
-	public void setup() {
-		this.inventoryDAO = new InventoryDAO(this.mongoTemplate);
-	}
+  @Before
+  public void setup() {
+    this.inventoryDAO = new InventoryDAO(this.mongoTemplate);
+  }
 
-	@After
-	public void tearDown() {
-		this.mongoTemplate.dropCollection(Inventory.class);
-	}
+  @After
+  public void tearDown() {
+    this.mongoTemplate.dropCollection(Inventory.class);
+  }
 
-	/**
-	 * Test Find All method.
-	 */
-	@Test
-	public void findAll() {
-		Inventory inventory = new Inventory();
-		inventory.setName(NAME);
-		inventory.setProductType(PRODUCT_TYPE);
-		this.mongoTemplate.save(inventory);
-		List<Inventory> actualInventory = this.inventoryDAO.findAll();
-		Assert.assertFalse(actualInventory.isEmpty());
-	}
+/**
+ * Test Find All method.
+ */
+  @Test
+  public void findAll() {
+    Inventory inventory = new Inventory();
+    inventory.setName(NAME);
+    inventory.setProductType(PRODUCT_TYPE);
+    this.mongoTemplate.save(inventory);
+    List<Inventory> actualInventory = this.inventoryDAO.findAll();
+    Assert.assertFalse(actualInventory.isEmpty());
+  }
 
-	/**
-	 * Test Create/Save method.
-	 */
-	@Test
-	public void save() {
-		// Creating new inventory objects. I have 2 inventories to see if their IDs are
-		// NOT same
-		Inventory inventory1 = new Inventory();
-		Inventory inventory2 = new Inventory();
+/**
+ * Test Create/Save method.
+ */
+  @Test
+  public void save() {
+    Inventory inventory1 = new Inventory();
+    Inventory inventory2 = new Inventory();
 
-		// Print to make sure inventories are null.
-		System.out.println("IB - inventory1 before saving data " + inventory1);
-		System.out.println("IB - inventory2 before saving data " + inventory2);
+    inventory1.setName("Veggies");
+    inventory1.setProductType("Potato");
 
-		// Setting name and product type for inventory1
-		inventory1.setName("Veggies");
-		inventory1.setProductType("Potato");
+    inventory2.setName("Fruits");
+    inventory2.setProductType("Apple");
 
-		// Setting name and product type for inventory2
-		inventory2.setName("Fruits");
-		inventory2.setProductType("Apple");
+    Inventory createdInventory1 = inventoryDAO.create(inventory1);
+    Inventory createdInventory2 = inventoryDAO.create(inventory2);
 
-		// Saving mongo template with inventories data with "create" method from
-		// InventoryDAO
-		Inventory createdInventory1 = inventoryDAO.create(inventory1);
-		Inventory createdInventory2 = inventoryDAO.create(inventory2);
+    Assert.assertNotNull("Inventory1 should NOT be Null", createdInventory1);
+    Assert.assertNotNull("Inventory2 should NOT be Null", createdInventory2);
 
-		// Test if inventory is not blank
-		Assert.assertNotNull("Inventory1 should NOT be Null", createdInventory1);
-		Assert.assertNotNull("Inventory2 should NOT be Null", createdInventory2);
+    Assert.assertNotNull("ID should NOT be Null", createdInventory1.getId());
+    Assert.assertNotNull("ID2 should NOT be Null", createdInventory2.getId());
 
-		// Test if ID is NOT null
-		Assert.assertNotNull("ID should NOT be Null", createdInventory1.getId());
-		Assert.assertNotNull("ID2 should NOT be Null", createdInventory2.getId());
+    Assert.assertEquals(createdInventory1.getName(), "Veggies");
+    Assert.assertEquals(createdInventory1.getProductType(), "Potato");
 
-		// Print to see inventories are saved with given data
-		System.out.println("Hi IB, your createdInventory1 is " + createdInventory1);
-		System.out.println("Hi IB, your createdInventory2 is " + createdInventory2);
+    Assert.assertEquals(createdInventory2.getName(), "Fruits");
+    Assert.assertEquals(createdInventory2.getProductType(), "Apple");
 
-		// Test to check inventory1 data is equal to saved one
-		Assert.assertEquals(createdInventory1.getName(), "Veggies");
-		Assert.assertEquals(createdInventory1.getProductType(), "Potato");
+    List<Inventory> actualInventory = this.inventoryDAO.findAll();
 
-		// Test to check inventory2 data is equal to saved one
-		Assert.assertEquals(createdInventory2.getName(), "Fruits");
-		Assert.assertEquals(createdInventory2.getProductType(), "Apple");
+    Assert.assertEquals(actualInventory.stream().count(), 2);
 
-		// To see all instances of current collection
-		List<Inventory> actualInventory = this.inventoryDAO.findAll();
-		// totalNumberOfInstances = actualInventory.stream().count();
-		System.out.println("IB - You inventory count is " + actualInventory.stream().count());
+  }
 
-		// Test is number of instances matches created ones. I have inventory1 and
-		// inventory2. So count should be equal to 2
-		Assert.assertEquals(actualInventory.stream().count(), 2);
+/**
+ * Test Delete method.
+ */
+  @Test
+  public void delete() { 
+    Inventory inventory = new Inventory();
+    inventory.setName("Toyota");
+    inventory.setProductType("Cars");
+    Inventory createdInventory = inventoryDAO.create(inventory);
+    System.out.println("Inventory before delete is " + inventory);
+    
+    Assert.assertNotNull("Inventory should NOT be Null", createdInventory);
+    Assert.assertNotNull("ID should NOT be Null", createdInventory.getId());
 
-		// this.inventoryDAO.delete(createdInventory.get()); - no need for Deleting
-		// collection, as it will be deleted with @After
+    inventory = this.inventoryDAO.delete(createdInventory.getId());  
+    System.out.println("Inventory after delete is " + inventory);
 
-		
-	}
+    Assert.assertNull(inventory);
+  
+  }
 }
